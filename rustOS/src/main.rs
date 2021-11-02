79bec3d02ccd9a250f4008f1fe2629baca051c37
Weber, Daniel <daniel.weber@dlr.de>
2021-11-03 00:00:00 +0100

    update

--- rustOS/src/main.rs
+++ rustOS/src/main.rs
@@ -28,11 +28,25 @@ fn panic(_info: &PanicInfo) -> ! {
 // requires some complex operating system capabilities, so we must
 // remove these capabilities. This is donein the Cargo.toml file
 
+static HELLO: &[u8] = b"Hello World!";
 
 // implement the starting point
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+  // this is the place where the vga buffer is located
+  let vga_buffer = 0xb8000 as *mut u8;
+
+  // for each character ..
+  for (i, &byte) in HELLO.iter().enumerate() {
+    unsafe {
+      // ... we do an unsafe write to the vga buffer
+      *vga_buffer.offset(i as isize * 2) = byte;
+      *vga_buffer.offset(i as isize * 2 + 1) = 0xb
+      // ... though this could be done better later.
+    }
+  }
+
   loop { }
 }
 
